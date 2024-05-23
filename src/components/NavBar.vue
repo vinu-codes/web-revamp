2f194c5d0b30499cdd76ac3975440c934be7986e
Vinu <vinujithmin@gmail.com>
2024-05-23 15:55:16 +1000

    initial routes

---
+++ src/components/NavBar.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <h1>Hello App!</h1>
+  <nav>
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/about">Go to About</RouterLink>
+  </nav>
+</template>
