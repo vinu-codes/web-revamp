c7dc74e51e024c7627bcff3f18c1af43bb29d990
Vinu <vinujithmin@gmail.com>
2024-05-24 11:33:25 +1000

    add favicon

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup>
 import { ref, isRef, reactive, toRefs, customRef } from "vue";
 import { RouterLink, RouterView } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
-import AboutView from "@/views/AboutView.vue";
 import NavBar from "@/components/NavBar.vue";
 </script>
 
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,9 +3,34 @@ import { RouterLink } from "vue-router";
 </script>
 
 <template>
-  <h1>Hello App!</h1>
-  <nav>
+  <nav class="nav-bar">
+    <h1>Hello App!</h1>
     <RouterLink to="/">Go to Home</RouterLink>
     <RouterLink to="/about">Go to About</RouterLink>
   </nav>
 </template>
+
+<style>
+.nav-bar {
+  display: flex;
+  padding: 1rem;
+  background-color: white;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  color: black;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+
+  h1 {
+    padding: 0;
+    margin: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: black;
+    padding: 0;
+  }
+}
+</style>
